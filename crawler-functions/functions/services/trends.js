b24145da472e6bb0c4d16cfcb1516cb15512ef40
Marcelio Santana <marcelio.santana@riachuelo.com.br>
2021-08-31 14:13:17 -0300

    refact() eslint config babel-eslint

--- crawler-functions/functions/services/trends.js
+++ crawler-functions/functions/services/trends.js
@@ -1,19 +1,19 @@
-const TikTokScraper  = require('tiktok-scraper');
-const ssidTt  = require('./config/ssid_tt');
+const TikTokScraper = require("tiktok-scraper");
+const ssidTt = require("./config/ssid_tt");
 
 // Trending feed
 const trendingTopicsService = async (data) => {
-    try {
-      const posts = await TikTokScraper.trend('viagem', {
-        number: 100,
-        sessionList: [`sid_tt=${ssidTt}`],
-      });
-      console.log('tamanho:: ', posts.collector.length);
-      return posts.collector;
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }  
+  try {
+    const posts = await TikTokScraper.trend("viagem", {
+      number: 100,
+      sessionList: [`sid_tt=${ssidTt}`],
+    });
+    console.log("tamanho:: ", posts.collector.length);
+    return posts.collector;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 
 export default trendingTopicsService;
